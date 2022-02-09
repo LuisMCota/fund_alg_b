8973f0e64d9f642a0cd737e381881d39760105f7
LuisMCota <luis.f.m.c@outlook.com>
2022-02-09 11:09:26 -0600

    argv lesson

--- unit1/variablesGlobal.c
+++ unit1/variablesGlobal.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-//*Global Variables*//
-char* date = "02-02-22T10:25:00z";
-///////////////////////////////////////
 
-///La ejecucion del Programa/////
+//*Global Variables*//
+char* date = "02-0222T10:25:00z";
+/////////////////////////////////
+
+/// La ejecucion del programa ///
+
 int main(){
     date = "02-03-22T10:25:00z";
-    int hours = 22; //Local:main %d-i%
+    int hours = 22; //Local:main   %d-%1
     float pi = 3.1416; //%f
     char character = 'c'; //%c
-    char* name = "LuisCota"; //%s
-    int *address = &hours; //%p
+    char* name = "Luis Cota"; //%s
+    int *address = &hours; //p
+    hours = hours + 2;
     printf("hours: %d, %s\n", hours, name);
     return 0;
 }
+
