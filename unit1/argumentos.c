fef076c609357c3d93392005ab3f3c672630e282
LuisMCota <luis.f.m.c@outlook.com>
2022-02-09 11:31:32 -0600

    commit all

--- unit1/argumentos.c
+++ unit1/argumentos.c
@@ -2,5 +2,7 @@
 #include "utils.h"
 
 int main(){
+    int isGreater = isGreaterThan10(11);
+    printf("The Number is: %d", isGreater);
     return 0;
 }
--- unit1/argv_argc.c
+++ unit1/argv_argc.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv){
     if(argc <= 1) return 0;
-    printf("Program executed: %s, number of inputs elements: %d", argv[0], (argc-1));
+    printf("Program executed: %s, number of inputs elements: %d\n", argv[0], (argc-1));
     printf("Hola %s %s %s %s, bienvenido!\n", argv[1], argv[2], argv[3], argv[4]);
     return 0;
 }
