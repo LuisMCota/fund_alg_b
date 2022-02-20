e48aa75ed350ad2c9792836ce0ceac3493e637b5
LuisMCota <luis.f.m.c@outlook.com>
2022-02-19 20:03:08 -0600

    bool

--- unit1/evenOdd.c
+++ unit1/evenOdd.c
@@ -1,15 +1,17 @@
-#include <stdio>
-#include <stdcool.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 
 
-int main(){
+int main(int argc, char** argv){
     int numberToTest = 0;
     int result = numberToTest % 2;
     bool hasModule = (result > 0);
 
     if(hasModule){
-
+        return result;
 } else{
+    printf("Number")
+}
 
 }
