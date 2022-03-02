5ca42ffc5bc7abb281f4d934b464507dbb24aa31
LuisMCota <luis.f.m.c@outlook.com>
2022-03-02 10:15:30 -0600

    commit

--- unit2/loops.c
+++ unit2/loops.c
@@ -6,5 +6,12 @@ int main(int argc, char** argv){
         printf("ITERATOR: %d\n", iterator);//la %d para que imprima el resultado del for con Iterator
         sleep(1000);// se encuentra en utils.h y sirve para que el programa corra mas lento = un segundo
     }
+    int rango = 1;
+    int count = 0 ;
+    while(rango <= 99){
+        count = count + rango;
+        rango++;
+    }
+    printf("suma: %d\n", count);
     return 0;
 }
